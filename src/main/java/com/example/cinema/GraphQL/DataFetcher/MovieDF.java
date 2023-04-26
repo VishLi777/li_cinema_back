99d8d0b9a2e82e42d3a47421af907387ffd0a20c
oslavskayaly <oslavskaya.l.y@edu.mirea.ru>
2023-04-26 14:34:34 +0300

    /editMovie added

--- src/main/java/com/example/cinema/GraphQL/DataFetcher/MovieDF.java
+++ src/main/java/com/example/cinema/GraphQL/DataFetcher/MovieDF.java
@@ -1,5 +1,6 @@
 package com.example.cinema.GraphQL.DataFetcher;
 
+import com.example.cinema.Entity.Hall;
 import com.example.cinema.Entity.Movie;
 import com.example.cinema.Service.MovieService;
 import graphql.schema.DataFetcher;
@@ -23,6 +24,25 @@ public class MovieDF {
 
     public DataFetcher<List<Movie>> getAll(){
         return env -> movieService.getAll();
+    }
+
+    public DataFetcher<Movie> editMovie(){
+        return env -> {
+            Long id = env.getArgument("id");
+            Movie movie = movieService.getById(id);
+            if(env.containsArgument("name"))
+                movie.setName(env.getArgument("name"));
+            if(env.containsArgument("author"))
+                movie.setAuthor(env.getArgument("author"));
+            if(env.containsArgument("duration"))
+                movie.setDuration(env.getArgument("duration"));
+            if(env.containsArgument("start_date_of_rental"))
+                movie.setStart_date_of_rental(env.getArgument("start_date_of_rental"));
+            if(env.containsArgument("end_date_of_rental"))
+                movie.setEnd_date_of_rental(env.getArgument("end_date_of_rental"));
+            return movieService.save(movie);
+        };
+
     }
 
 }
